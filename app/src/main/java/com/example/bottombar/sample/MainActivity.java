d60b28a03d387456c17ec8c15d91b1189ac2fd5a
Somaya-Mofarjy <435201668@student.ksu.edu.sa>
2017-07-16 10:09:40 +0300

    z

--- app/src/main/java/com/example/bottombar/sample/MainActivity.java
+++ app/src/main/java/com/example/bottombar/sample/MainActivity.java
@@ -1,50 +1,61 @@
 package com.example.bottombar.sample;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+public class MainActivity extends AppCompatActivity {
+// ImageView m= (ImageView) findViewById(R.id.nouf);
+
+    BottomBar bottomBar ;
     @SuppressWarnings("ConstantConditions")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.simple_three_tabs).setOnClickListener(this);
-        findViewById(R.id.icons_only).setOnClickListener(this);
-        findViewById(R.id.five_tabs_changing_colors).setOnClickListener(this);
-        findViewById(R.id.three_tabs_quick_return).setOnClickListener(this);
-        findViewById(R.id.five_tabs_custom_colors).setOnClickListener(this);
-        findViewById(R.id.badges).setOnClickListener(this);
+
+
+        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setDefaultTab(R.id.tab_home);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_info) {
+                    TextView t = (TextView)findViewById(R.id.text);
+                    t.setText("دليل!!");
+
+                    //m.setVisibility(View.VISIBLE);
+
+                }else if(tabId == R.id.tab_home){
+
+                    //m.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+
+
+        //findViewById(R.id.five_tabs_changing_colors).setOnClickListener(this);
+
     }
 
-    @Override
+
+
+/*    @Override
     public void onClick(View v) {
         Class clazz = null;
-
         switch (v.getId()) {
-            case R.id.simple_three_tabs:
-                clazz = ThreeTabsActivity.class;
-                break;
-            case R.id.icons_only:
-                clazz = IconsOnlyActivity.class;
-                break;
             case R.id.five_tabs_changing_colors:
                 clazz = FiveColorChangingTabsActivity.class;
                 break;
-            case R.id.three_tabs_quick_return:
-                clazz = ThreeTabsQRActivity.class;
-                break;
-            case R.id.five_tabs_custom_colors:
-                clazz = CustomColorAndFontActivity.class;
-                break;
-            case R.id.badges:
-                clazz = BadgeActivity.class;
-                break;
         }
-
         startActivity(new Intent(this, clazz));
-    }
-}
+    }*/
+
+}//end class
